3410767de76e674fa12e2fcca8553df93f50395b
서원우 <mistersoh@naver.com>
2023-03-13 20:24:51 +0900

    [level 3] Title: 정수 삼각형, Time: 34.38 ms, Memory: 14.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43105. 정수 삼각형/정수 삼각형.py
@@ -0,0 +1,11 @@
+def solution(triangle):
+    answer = 0
+    dp = []
+    
+    for i in range(len(triangle)-1,0,-1):
+        for j in range(i):
+            triangle[i-1][j] += max(triangle[i][j], triangle[i][j+1])
+            
+        
+    
+    return triangle[0][0]
